c69e304af4a68b10ff0727f1d70888c3abd503fb
Habiesu <javier.lopez3004@gmail.com>
2023-01-27 02:14:17 -0400

    Update 1.0.6

--- Proyectos/buscarEnLaPagina/index.js
+++ Proyectos/buscarEnLaPagina/index.js
@@ -1,5 +1,6 @@
 var input = document.getElementById('busqueda');
 var parrafosArray = Array.from(document.getElementsByTagName('p'));
+let parrafosOriginales = [];
 
 document.getElementById('busqueda').addEventListener('keyup', (a) => {
     verificarBusqueda()
@@ -8,15 +9,27 @@ document.getElementById('busqueda').addEventListener('keyup', (a) => {
 function verificarBusqueda(){
     if(input.value.trim()){
         for (let i = 0; i < parrafosArray.length; i++) {
-            let parrafo = parrafosArray[i].innerText;
-            let buscar = input.value;
-            if (parrafo.indexOf(buscar) != -1){
-                    parrafosArray[i].innerHTML = parrafo.replace(buscar, `<span class="bg-red-900 text-white">${buscar}</span>`)
+            if(!parrafosOriginales[i]){
+                parrafosOriginales[i] = parrafosArray[i].innerText;
             }
+            let buscar = input.value;
+            if (parrafosOriginales[i].indexOf(buscar) != -1){
+                parrafosArray[i].innerHTML = parrafosOriginales[i].replace(new RegExp(buscar, "g"), `<span class="bg-red-900 text-white">${buscar}</span>`);
+            } 
         }   
+    }else{
+        for (let i = 0; i < parrafosArray.length; i++) {
+            parrafosArray[i].innerHTML = parrafosOriginales[i];
+        }
     }
 }
 
-/*
-Falta por terminar el que se cambien los spans por parrafos nuevamente.
+/* Tristemente tuve ayuda de chatgpt para terminar la parte lógica de eliminar los elementos span, yo ya tenía la idea de almacenarlos en una variable pero no sabía o no llegué al resultado que era usando:
+
+else{
+        for (let i = 0; i < parrafosArray.length; i++) {
+            parrafosArray[i].innerHTML = parrafosOriginales[i];
+        }
+    }
+
 */
